32fe58081d9a6a80dd98946a120601d870cc2c37
torahz <rafaeltorahz@gmail.com>
2024-08-02 15:08:40 -0300

    Alterações
    - implementação de formato app
    - correções gerais

--- sw.js
+++ sw.js
@@ -1,7 +1,30 @@
+const CACHE_NAME = 'whatsapp-sem-contato-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json'
+];
+
 self.addEventListener('install', function(event) {
-  console.log('Service Worker: Installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
